2e74fa0d90b1b8130e30a82ea0af0877bd85841a
dormi330 <dormi330@gmail.com>
2015-03-06 14:12:40 +0800

    继续修改

--- mytest.js
+++ mytest.js
@@ -5,15 +5,22 @@ app.listen(3000);
 
 app.use(function(req, res, next) {
 	console.log('exe mid-ware log', req.url);
+	// add a mid-ware dynamiclly, 
+	app.use(function(req, res, next) {
+		console.log('exe mid-ware log2', req.url);
+		next();
+	});
 	next();
 });
 
 app.use('/x/yz', function(req, res, next) {
 	console.log('exe mid-ware /x/yz', req.url);
-	res.end('hello, ok' + req.url);
+	next();
+	// res.end('hello, ok' + req.url);
 });
 
 app.use('/', function(req, res, next) {
 	console.log('exe mid-ware /', req.url);
-	res.end('hello, ok' + req.url);
+	// res.end('hello, ok' + req.url);
+	next();
 });
