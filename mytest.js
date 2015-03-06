b578335c192bd424a5524957888b36edbd071f10
dormi330 <dormi330@gmail.com>
2015-03-06 13:44:35 +0800

    增加一些注释，明晰流程

---
+++ mytest.js
@@ -0,0 +1,19 @@
+var connect = require('./index.js');
+
+var app = connect();
+app.listen(3000);
+
+app.use(function(req, res, next) {
+	console.log('exe mid-ware log', req.url);
+	next();
+});
+
+app.use('/x/yz', function(req, res, next) {
+	console.log('exe mid-ware /x/yz', req.url);
+	res.end('hello, ok' + req.url);
+});
+
+app.use('/', function(req, res, next) {
+	console.log('exe mid-ware /', req.url);
+	res.end('hello, ok' + req.url);
+});
